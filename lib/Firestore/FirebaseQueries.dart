cdc8a66bd396d17e621dd44f95b2de87579ba2b8
Kyle S <shawkyle06@yahoo.com>
2023-12-08 16:40:06 -0500

    UpdatedCommit

---
+++ lib/Firestore/FirebaseQueries.dart
@@ -0,0 +1,138 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:snhu_tutorlink/Models/TutorAvailabilityCard.dart';
+import 'package:intl/intl.dart';
+
+import '../Models/Availability.dart';
+import '../Models/Tutor.dart';
+
+class FirebaseQueries{
+  static int readsCounter = 0;
+  final db = FirebaseFirestore.instance;
+
+  final course = <String, String>{
+    "courseName": "Intro To Junior Software Engineering"
+  };
+
+  void addCourse(){
+    db.collection("Courses").doc("CS315").set(course);
+  }
+
+  Future<List<Availability>> getTutorAvailabilities(String tutorID) async {
+    List<Availability> availabilities = [];
+    final ref = db.collection("Availability").where("Tutor", isEqualTo: tutorID).withConverter(
+        fromFirestore: Availability.fromFirestore,
+        toFirestore: (Availability availibility, _) => availibility.toFirestore());
+    final result = await ref.get().then((querySnapshot){
+      for(var docsnapshot in querySnapshot.docs){
+        final result = docsnapshot.data();
+        print(result);
+        availabilities.add(result);
+      }
+    });
+    return availabilities;
+  }
+
+  Future<List<Map<String, dynamic>>> searchAvailibilitiesOnDate(DateTime time) async {
+    DateTime startDate = DateTime(time.year, time.month, time.day);
+    DateTime endDate = time.add(const Duration(days:1));
+    
+    List<Map<String, dynamic>> availableTutors = [];
+    await db.collection("Availability").where('Start_Time', isGreaterThanOrEqualTo:startDate)
+        .where('Start_Time', isLessThanOrEqualTo:endDate).get().then((querySnapshot){
+      if(querySnapshot != null){
+        for(var docsnapshot in querySnapshot.docs){
+          readsCounter++;
+          print("Document:Availability Total Firestore Reads: $readsCounter");
+          availableTutors.add(docsnapshot.data());
+        }
+      }
+    });
+    return availableTutors;
+  }
+  Future<List<Availability>> nonRepeatAvailibilitiesOnDate(DateTime time) async {
+    DateTime startDate = DateTime(time.year, time.month, time.day);
+    DateTime endDate = time.add(const Duration(days:1));
+
+    final ref = db.collection("Availability").where('Start_Time', isGreaterThanOrEqualTo:startDate)
+        .where('Start_Time', isLessThanOrEqualTo:endDate).where("IsRepeating", isEqualTo: false).withConverter(
+        fromFirestore: Availability.fromFirestore,
+        toFirestore: (Availability availibility, _) => availibility.toFirestore());
+
+    List<Availability> availableTutors = [];
+    await ref.get().then((querySnapshot){
+      if(querySnapshot != null){
+        for(var docsnapshot in querySnapshot.docs){
+          readsCounter++;
+          print("Document:Availability Total Firestore Reads: $readsCounter");
+          availableTutors.add(docsnapshot.data());
+        }
+      }
+    });
+    return availableTutors;
+  }
+  Future<List<Availability>> repeatAvailibilitiesOnDate(DateTime time) async {
+    String dateAbbr = DateFormat('E').format(time).substring(0,2).toUpperCase();
+    DateTime currentDate = DateTime(time.year, time.month, time.day);
+    DateTime startDate = DateTime(time.year, time.month, time.day).add(const Duration(days: 1));
+    DateTime endDate = time.add(const Duration(days:1));
+
+    final ref = db.collection("Availability").where('Start_Time', isLessThanOrEqualTo:startDate)
+        .where('DateAbbreviation', isEqualTo: dateAbbr).withConverter(
+        fromFirestore: Availability.fromFirestore,
+        toFirestore: (Availability availibility, _) => availibility.toFirestore());
+
+    List<Availability> availableTutors = [];
+    await ref.get().then((querySnapshot){
+      if(querySnapshot != null){
+        for(var docsnapshot in querySnapshot.docs){
+          readsCounter++;
+          print("Document:Availability Total Firestore Reads: $readsCounter");
+
+          bool isActive = docsnapshot.data().endDate?.isAfter(currentDate) ?? false;
+          if(isActive){
+            availableTutors.add(docsnapshot.data());
+          }
+        }
+      }
+    });
+    return availableTutors;
+  }
+
+  Future<List<TutorAvailabilityCard>> getTutorInformation(List<Availability> availabilities) async {
+    List<TutorAvailabilityCard> availabilityCards = [];
+    for(var availability in availabilities){
+      final ref = db.collection("Tutor").doc(availability.tutorReference).withConverter(
+          fromFirestore: Tutor.fromFirestore,
+          toFirestore: (Tutor tutor, _) => tutor.toFirestore());
+
+      await ref.get().then((querySnapshot){
+        if(querySnapshot != null){
+          print(querySnapshot);
+
+          var tutor = querySnapshot?.data();
+          if(tutor != null){
+            readsCounter++;
+            print("Document:Tutor Total Firestore Reads: $readsCounter");
+            availabilityCards.add(TutorAvailabilityCard(tutor: tutor, availibility: availability));
+          }
+        }
+      });
+    }
+    return availabilityCards;
+  }
+
+  Future<List<TutorAvailabilityCard>> getAvailabilitiesOnDate(DateTime date) async {
+    List<Availability> availabilitiesOnDate = await nonRepeatAvailibilitiesOnDate(date);
+    availabilitiesOnDate = await repeatAvailibilitiesOnDate(date);
+    List<TutorAvailabilityCard> tutorAvailabilityCards = await getTutorInformation(availabilitiesOnDate);
+    return tutorAvailabilityCards;
+  }
+
+  Future<List<String>> getUnavailableTimeslots(String tutorID, String docRef, String date) async{
+    List<String> unavailableTimeslots = [];
+    final ref = db.collection("Availability").doc(docRef);
+
+    return unavailableTimeslots;
+  }
+
+}
--- lib/userData.dart
+++ lib/userData.dart
@@ -1,3 +1,8 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
@@ -8,9 +13,36 @@ class UserData extends ChangeNotifier {
   File? get image => _image;
   String? get imageUrl => _imageUrl;
 
-  void setImage(File image, String imageUrl) {
-    _image = image;
-    _imageUrl = imageUrl;
+  Future<void> loadProfileImage() async{
+    final FirebaseAuth auth = FirebaseAuth.instance;
+
+    try {
+      final destination = 'public/images/${auth.currentUser?.uid ?? ""}';
+
+      final ref = FirebaseStorage.instance
+          .ref()
+          .child(destination);
+      _imageUrl = await ref.getDownloadURL();
+    } on FirebaseException catch (e) {
+      print("Failed with error '${e.code}': ${e.message}");
+    }
+
+    if(_imageUrl != null){
+      Image image = Image.network(_imageUrl ?? "");
+    }else{
+      _imageUrl = "...";
+    }
+
     notifyListeners();
   }
+
+  Future<void> setImage(File image, String imageUrl) async {
+    _image = image;
+    _imageUrl = imageUrl;
+
+    notifyListeners();
+  }
+
+
+
 }
